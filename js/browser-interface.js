dfe022e8a898ee22f533a74e037774fb1463c9fd
Christine Tacha and Oskar Radon <oskarradonkimball@gmail.com>
2016-03-14 15:58:46 -0700

    Add pseudocode for UI logic

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -2,3 +2,25 @@ var simon = require('./../js/game-interface.js').simon;
 
 
 // function to push user input to userArray
+
+$(function() {
+  $('.newGame').click(function() {
+
+    // run compareArrays
+
+      // if gameLoss = false
+      
+        // run generateColor
+
+        // highlight corresponding div in sequence of simonArray
+
+        // switch turn
+
+        // when user clicks div, put that color into userArray
+
+        // once user div clicks = simonArray.length
+
+    // else show lose message
+
+  });
+});
